6669998dbd315f38687905cf6d3a547f9bee73e5
debikus <chekov01denus@gmail.com>
2023-05-13 12:53:32 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        SortedList<Record, string> records = new SortedList<Record, string>();
+
+        records.Add(new Record("Чеков", "38067155232", "Галицька 133"), "Замітка 1");
+        records.Add(new Record("Власов", "38067152589", "Стрийська 9"), "Замітка 2");
+        records.Add(new Record("Чернецький", "38093255241", "Наукова 5"), "Замітка 3");
+        records.Add(new Record("Максвел", "38093719273", "Княгинi Ольги 91"), "Замітка 4");
+
+        Console.WriteLine("Всi записи:");
+        foreach (var item in records)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        records.Remove(records.Keys[0]);
+        Console.WriteLine("\nЗаписи пiсля видалення першого:");
+        foreach (var item in records)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream fs = new FileStream("records.dat", FileMode.Create))
+        {
+            formatter.Serialize(fs, records);
+        }
+        using (FileStream fs = new FileStream("records.dat", FileMode.Open))
+        {
+            SortedList<Record, string> deserializedRecords = (SortedList<Record, string>)formatter.Deserialize(fs);
+
+            Console.WriteLine("\nЗаписи пiсля десерiалiзацiї:");
+            foreach (var item in deserializedRecords)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Record.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+[Serializable]
+public class Record : IComparable<Record>
+{
+    public string Surname { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+
+    public Record(string surname, string phoneNumber, string address)
+    {
+        Surname = surname;
+        PhoneNumber = phoneNumber;
+        Address = address;
+    }
+
+    public int CompareTo(Record other)
+    {
+        return this.Surname.CompareTo(other.Surname);
+    }
+
+    public override string ToString()
+    {
+        return $"Прiзвище: {Surname}, Номер телефону: {PhoneNumber}, Адреса: {Address}";
+    }
+}
